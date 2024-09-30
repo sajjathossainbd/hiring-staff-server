ea5c44237a0a32aca1d274c154fb98ab8c6a55fb
Israfil6580 <israfilhossainmilon@gmail.com>
2024-09-30 09:19:29 +0600

    to see the code just

--- index.js
+++ index.js
@@ -36,5 +36,5 @@ app.get("/", (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port is ${port}`);
 });

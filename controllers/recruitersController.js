4958f688fd457ec397d470a8c6607cfa46c9806f
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2024-10-03 17:17:38 +0600

    update  github

--- controllers/recruitersController.js
+++ controllers/recruitersController.js
@@ -1,66 +1,22 @@
+// controllers/recruitersController.js
+
 const { client, ObjectId } = require("../config/db");
 const recruitersCollection = client
   .db("hiringStaffDB")
   .collection("recruiters");
 
 // GET all recruiters
-// search by jobTitle and category
 exports.getAllRecruiters = async (req, res) => {
-  const { jobTitle, category } = req.query;
-
-  const query = {};
-
-  if (jobTitle || category) {
-    const jobPostingsQuery = { $or: [] };
-
-    if (jobTitle) {
-      jobPostingsQuery.$or.push({
-        "jobPostings.jobTitle": {
-          $regex: jobTitle,
-          $options: "i",
-        },
-      });
-    }
-
-    if (category) {
-      jobPostingsQuery.$or.push({
-        "jobPostings.category": {
-          $regex: category,
-          $options: "i",
-        },
-      });
-    }
-
-    query.$or = [jobPostingsQuery];
-  }
-
-  try {
-    const result = await recruitersCollection.find(query).toArray();
-
-    res.send(result);
-  } catch (error) {
-    console.error("Error fetching recruiters: ", error);
-    res
-      .status(500)
-      .send({ message: "An error occurred while fetching recruiters.", error });
-  }
+  const result = await recruitersCollection.find().toArray();
+  // console.log(result);
+  res.send(result);
 };
 
 // GET a specific recruiter by ID
 exports.getRecruiterById = async (req, res) => {
   const recruiterId = req.params.id;
-  try {
-    const result = await recruitersCollection.findOne({
-      _id: new ObjectId(recruiterId),
-    });
-
-    if (!result) {
-      return res.status(404).send({ message: "Recruiter not found." });
-    }
-
-    res.send(result);
-  } catch (error) {
-    console.error("Error fetching recruiter by ID: ", error);
-    res.status(500).send({ message: "An error occurred.", error });
-  }
+  const result = await recruitersCollection.findOne({
+    _id: new ObjectId(recruiterId),
+  });
+  res.send(result);
 };

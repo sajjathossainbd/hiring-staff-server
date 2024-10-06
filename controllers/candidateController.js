ddff9d9662ee5b3983e94f6f12a96c579c5476a4
KhEliash <eliashebrahim@gmail.com>
2024-10-06 21:40:36 +0600

    add: added candidate search and filtering methode

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -21,6 +21,7 @@ exports.getAllCandidates = async (req, res) => {
 
 // Get candidate by id
 exports.getCandidateById = async (req, res) => {
+  console.log(req.params);
   const id = req.params.id;
 
   if (!ObjectId.isValid(id)) {
@@ -38,5 +39,56 @@ exports.getCandidateById = async (req, res) => {
   } catch (error) {
     console.error("Error fetching candidate by ID:", error);
     sendResponse(res, { message: "Failed to fetch candidate" }, 500);
+  }
+};
+
+// search and filter candidates
+
+exports.searchAndFilterCandidates = async (req, res) => {
+  try {
+    const profession = req.query.profession || "";
+    const location = req.query.location || "";
+    const skills = req.query.skills || "";
+    const experience = req.query.experience || "";
+    const education = req.query.education || "";
+    const jobType = req.query.jobType || "";
+
+    const query = {};
+ 
+    if (profession) {
+      query.special_profession = { $regex: profession, $options: "i" };
+    }
+
+    if (location) {
+      query["location.city"] = { $regex: location, $options: "i" };
+    }
+
+    if (skills) {
+      const skillArray = skills.split(",").map((skill) => skill.trim());
+      query.skills = {
+        $all: skillArray.map((skill) => new RegExp(skill, "i")),
+      };
+    }
+
+    if (experience) {
+      query.experience_year = parseInt(experience);
+    }
+
+    if (education) {
+      query["education.degree"] = { $regex: education, $options: "i" };
+    }
+
+    if (jobType) {
+      if (jobType === "remote") {
+      } else if (jobType === "onsite") {
+      } else if (jobType === "hybrid") {
+      }
+    }
+
+    const candidates = await candidatesCollection.find(query).toArray();
+
+    res.json(candidates);
+  } catch (err) {
+    res.status(500).send("Server Error");
   }
 };

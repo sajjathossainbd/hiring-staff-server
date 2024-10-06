65738015d0955af2a4cbb69d855b4d7aafc12e81
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2024-10-06 11:07:46 +0600

    github update

--- controllers/recruitersController.js
+++ controllers/recruitersController.js
@@ -6,46 +6,10 @@ const recruitersCollection = client
   .collection("recruiters");
 
 // GET all recruiters
-// search by jobTitle and category
 exports.getAllRecruiters = async (req, res) => {
-  const { jobTitle, category } = req.query;
-
-  const query = {};
-
-  if (jobTitle || category) {
-    const jobPostingsQuery = { $or: [] };
-
-    if (jobTitle) {
-      jobPostingsQuery.$or.push({
-        "jobPostings.jobTitle": {
-          $regex: jobTitle,
-          $options: "i",
-        },
-      });
-    }
-
-    if (category) {
-      jobPostingsQuery.$or.push({
-        "jobPostings.category": {
-          $regex: category,
-          $options: "i",
-        },
-      });
-    }
-
-    query.$or = [jobPostingsQuery];
-  }
-
-  try {
-    const result = await recruitersCollection.find(query).toArray();
-
-    res.send(result);
-  } catch (error) {
-    console.error("Error fetching recruiters: ", error);
-    res
-      .status(500)
-      .send({ message: "An error occurred while fetching recruiters.", error });
-  }
+  const result = await recruitersCollection.find().toArray();
+  // console.log(result);
+  res.send(result);
 };
 
 // GET a specific recruiter by ID

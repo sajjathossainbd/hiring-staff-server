c6c92abc8b89ad43a7e082620566d3d3ccb48505
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2024-10-06 22:43:56 +0600

    github issues

--- controllers/recruitersController.js
+++ controllers/recruitersController.js
@@ -5,14 +5,14 @@ const recruitersCollection = client
   .db("hiringStaffDB")
   .collection("recruiters");
 
-// GET all recruiters
+
 exports.getAllRecruiters = async (req, res) => {
   const result = await recruitersCollection.find().toArray();
-  // console.log(result);
+  
   res.send(result);
 };
 
-// GET a specific recruiter by ID
+
 exports.getRecruiterById = async (req, res) => {
   const recruiterId = req.params.id;
   const result = await recruitersCollection.findOne({
@@ -20,3 +20,59 @@ exports.getRecruiterById = async (req, res) => {
   });
   res.send(result);
 };
+
+  
+exports.getAllRecruiters = async (req, res) => {
+  try {
+    const { page = 1, limit = 10, search = '', location = '', industry = '', organization = '', teamSize = '' } = req.query;
+
+    
+    const skip = (page - 1) * limit;
+    const query = {};
+
+   
+    if (search) {
+      query.name = { $regex: search, $options: 'i' }; 
+    }
+
+   
+    if (location) {
+      query['location.city'] = { $regex: location, $options: 'i' }; 
+    }
+
+    if (industry) {
+      query.industry = { $regex: industry, $options: 'i' }; 
+    }
+
+   
+    if (organization) {
+      query.name = { $regex: organization, $options: 'i' }; 
+    }
+
+
+    if (teamSize) {
+      const numberOfEmployees = parseInt(teamSize);
+      if (!isNaN(numberOfEmployees)) {
+        query.numberOfEmployees = numberOfEmployees;
+      }
+    }
+
+    
+    const result = await recruitersCollection.find(query).skip(skip).limit(parseInt(limit)).toArray();
+    
+ 
+    const totalCount = await recruitersCollection.countDocuments(query);
+    const totalPages = Math.ceil(totalCount / limit);
+
+    res.send({
+      data: result,
+      totalPages,
+      currentPage: parseInt(page),
+      totalCount
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Error fetching recruiters" });
+  }
+};
+

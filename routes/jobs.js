c5815ea103fa0affb87e790dd4cde1833acd0b42
dev-kawser <kawserofficial.dev@gmail.com>
2024-10-14 13:03:08 +0600

    Add: add api on recruiter for get jobs by email

--- routes/jobs.js
+++ routes/jobs.js
@@ -19,7 +19,6 @@ router.post("/", postJob);
 // get all jobs
 router.get("/categories", getUniqueCategories);
 router.get("/job_location", getUniqueLocation);
-router.get("/email/:email", getJobsByEmail);
 router.get("/", getAllJobsWithFilter);
 router.get("/:id", getJob);
 

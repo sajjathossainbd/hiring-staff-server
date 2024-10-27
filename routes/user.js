0f71deec107d90b6a355b4d7d58cb446589027c7
dev-kawser <kawserofficial.dev@gmail.com>
2024-10-28 01:06:00 +0600

    Add: add patch api for update candidate informations

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,6 @@ const router = express.Router();
 
 
 router.get("/current/:email", getCurrentUser);
-router.patch("/profile/:email", updateUserProfile);
 
 // get emails for sending email
 router.get("/candidate-emails", candidatesEmail);
